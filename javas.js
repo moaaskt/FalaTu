dd76b117b17ead79110733aead21d7afe61d1467
moaaskt <moacirmayer@hotmail.com>
2023-08-05 03:08:49 -0300

    att

--- javas.js
+++ javas.js
@@ -4,9 +4,12 @@ let falatuBtn = document.querySelector('.submit');
 let synth = speechSynthesis;
 let isSpeaking = true;
 
+
+
+
 function falatu() {
     for (let voice of synth.getVoices()) {
-        let option = document.createElement('option');
+        let option = document.createElement('option')
         option.text = voice.name;
         listaVoz.add(option);
     }
@@ -23,6 +26,10 @@ function falarTexto(text) {
     }
     speechSynthesis.speak(alternar);
 }
+
+
+
+
 
 falatuBtn.addEventListener('click', (e) => {
     e.preventDefault();
@@ -45,9 +52,26 @@ falatuBtn.addEventListener('click', (e) => {
                     isSpeaking = true;
                     falatuBtn.innerHTML = 'Fala tu';
                 }
-            }, 100); // Especifica o intervalo em milissegundos (100 ms no exemplo)
+            });
         } else {
             falatuBtn.innerHTML = 'Repetir fala';
         }
+
+        // Chamada da função converterTexto() após clicar no botão
+        converterTexto();
     }
 });
+
+function converterTexto() {
+    for (let voice of synth.getVoices()) {
+        if (voice.name === listaVoz.value) {
+            falarTexto(textArea.value);
+            const conversionMessage = document.getElementById('conversionMessage');
+            conversionMessage.style.display = 'block';
+            setTimeout(() => {
+                conversionMessage.style.display = 'none';
+            }, 2000);
+            break;
+        }
+    }
+}
